58d1a5d5803d013cf107e8d845a9e4bd21777c9d
mariyamhidayat <157726397+mariyamhidayat@users.noreply.github.com>
2024-08-21 16:59:59 +0500

    Add files via upload

---
+++ lib.py
@@ -0,0 +1,51 @@
+import string as s
+import math as m 
+print(" letters",s.ascii_letters)
+print("lowercase ",s.ascii_lowercase)
+print("uppercase",s.ascii_uppercase)
+print("hexadigits  ",s.hexdigits)
+print("punctuation",s.punctuation)
+print(" whitespace",s.whitespace)
+print("printable ",s.printable)
+
+print(m.ceil(10.4))# print greatest number
+print(m.asinh(18))
+print(m.atan(12))
+print(m.cos(90))
+print(m.fabs(-10))# absulte value)
+print(m.degrees(190))
+print(m.factorial(18))
+print(m.gcd(30,3)) # greatest common divisior 
+print(m.floor(80.34))# remove the values after point 
+print(m.exp(10))
+print(m.fsum([12,44,55,66,77,]))# sum the list 
+print(m.sqrt(49))
+
+# while loop 
+# sentinal loop unknown number of iterations 
+i=int(input(" enter the value of i "))
+while i!=0:
+    print(f"square of {i*i}")
+    i=int(input(" exist to 0 "))
+   
+print(  "by")
+
+# counter loop  number of iterations known 
+i=1
+while i<10:
+    print(i)
+    i=i+1
+print("bye")
+# if 
+
+
+
+
+
+
+
+
+
+
+
+
